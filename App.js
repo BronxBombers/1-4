6d3c2c6d9408bb9c2d1c50be9864f4842c525f5d
Zach Morgan <zachmorgan987221@gmail.com>
2018-02-24 15:23:18 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>One is Greater Than 4.</Text>
+        <Text>Sam is gey</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
