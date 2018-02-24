7b0794d6d7055d7b9f650dc4c9fbc86b43c7d55d
Zach Morgan <zachmorgan987221@gmail.com>
2018-02-24 17:58:12 -0500

    stars

--- App.js
+++ App.js
@@ -5,14 +5,44 @@ import { AppRegistry, Image, Button, Text, Alert, View, TouchableOpacity } from 
 export default class ratingApp extends Component {
   render() {
     return (
-      <View style={{flex:1, flexDirection:"row", backgroundColor:"blue", alignItems:"flex-end", marginBottom:50}}>
-        <TouchableOpacity
-          onPress={() => {
-          Alert.alert("hello");
-          }
-        }>
-          <Image source={require("./assets/star.png")} style={{height:100,width:100}}/>
-        </TouchableOpacity>
+      <View style={{flex:1,flexDirection:"column",backgroundColor:"blue",alignItems:"center"}}>
+        <View style={{flex:0, flexDirection:"row", backgroundColor:"blue", alignItems:"flex-end", marginBottom:60}}>
+          <TouchableOpacity
+            onPress={() => {
+            Alert.alert("hello");
+            }
+          }>
+            <Image source={require("./assets/star.png")} style={{height:100,width:100}}/>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+            Alert.alert("hello");
+            }
+          }>
+            <Image source={require("./assets/star.png")} style={{height:100,width:100}}/>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+            Alert.alert("hello");
+            }
+          }>
+            <Image source={require("./assets/star.png")} style={{height:100,width:100}}/>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+            Alert.alert("hello");
+            }
+          }>
+            <Image source={require("./assets/star.png")} style={{height:100,width:100}}/>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+            Alert.alert("hello");
+            }
+          }>
+            <Image source={require("./assets/star.png")} style={{height:100,width:100}}/>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
