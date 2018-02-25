0ed262b47d4f67eb8048df699fec82a23e4f7f3f
Zach Morgan <zachmorgan987221@gmail.com>
2018-02-24 19:31:53 -0500

    ui base done

--- App.js
+++ App.js
@@ -5,46 +5,49 @@ import { AppRegistry, Image, Button, Text, Alert, View, TouchableOpacity } from 
 export default class ratingApp extends Component {
   render() {
     return (
-      <View style={{flex:1,flexDirection:"column",backgroundColor:"red",justifyContent:"flex-end"}}>
-        <View style={{flex:0,flexDirection:"column", alignItems:"center",marginBottom:150,marginTop:100}}>
-          <Text style={{fontSize:36,paddingBottom:50}}>Store Name Here</Text>
-          <Image source={require("./assets/placeholder.jpg")} style={{height:250,width:250}}/>
+      
+      <View style={{flex:1,flexDirection:"column",backgroundColor:"blue",justifyContent:"space-between"}}>
+        <View style={{flex:0, alignItems:"center",marginTop:75}}>
+          <Text style={{fontSize:40,color:"#FFFFFF"}}>Store Name Here</Text>
         </View>
-        <View style={{flex:0, flexDirection:"row", backgroundColor:"blue", marginBottom:75}}>
+        <View style={{flex:0, alignItems:"center"}}>
+          <Image source={require("./assets/placeholder.jpg")} style={{height:250,width:250,borderRadius:125,borderWidth:10,borderColor:"#FFFFFF"}}/>
+        </View>
+        <View style={{flex:0, flexDirection:"row",justifyContent:"space-between", marginBottom:75}}>
           <TouchableOpacity
             onPress={() => {
             Alert.alert("hello");
             }
           }>
-            <Image source={require("./assets/star.png")} style={{height:75,width:75}}/>
+            <Image source={require("./assets/star.png")} style={{height:60,width:60}}/>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => {
             Alert.alert("hello");
             }
           }>
-            <Image source={require("./assets/star.png")} style={{height:75,width:75}}/>
+            <Image source={require("./assets/star.png")} style={{height:60,width:60}}/>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => {
             Alert.alert("hello");
             }
           }>
-            <Image source={require("./assets/star.png")} style={{height:75,width:75}}/>
+            <Image source={require("./assets/star.png")} style={{height:60,width:60}}/>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => {
             Alert.alert("hello");
             }
           }>
-            <Image source={require("./assets/star.png")} style={{height:75,width:75}}/>
+            <Image source={require("./assets/star.png")} style={{height:60,width:60}}/>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => {
             Alert.alert("hello");
             }
           }>
-            <Image source={require("./assets/star.png")} style={{height:75,width:75}}/>
+            <Image source={require("./assets/star.png")} style={{height:60,width:60}}/>
           </TouchableOpacity>
         </View>
       </View>
